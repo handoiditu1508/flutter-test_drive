f55390f9b307cdf34b51a50ea4c76d1689ee4f43
Danh <handoiditu1508@gmail.com>
2024-07-01 23:57:26 +0700

    center the content

--- lib/main.dart
+++ lib/main.dart
@@ -37,17 +37,20 @@ class MyHomePage extends StatelessWidget {
     var pair = appState.current;
 
     return Scaffold(
-      body: Column(
-        children: [
-          Text('A random AWESOME idea:'),
-          BigCard(pair: pair),
-          ElevatedButton(
-            onPressed: () {
-              print('button pressed!');
-            },
-            child: Text('Next'),
-          ),
-        ],
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text('A random AWESOME idea:'),
+            BigCard(pair: pair),
+            ElevatedButton(
+              onPressed: () {
+                print('button pressed!');
+              },
+              child: Text('Next'),
+            ),
+          ],
+        ),
       ),
     );
   }
