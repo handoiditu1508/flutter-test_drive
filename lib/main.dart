3c85bc2e48768b283a234b43962d179fdf5830d3
Danh <handoiditu1508@gmail.com>
2024-07-02 14:58:06 +0700

    add button behavior

--- lib/main.dart
+++ lib/main.dart
@@ -28,6 +28,11 @@ class MyApp extends StatelessWidget {
 
 class MyAppState extends ChangeNotifier {
   var current = WordPair.random();
+
+  void getNext() {
+    current = WordPair.random();
+    notifyListeners();
+  }
 }
 
 class MyHomePage extends StatelessWidget {
@@ -44,7 +49,7 @@ class MyHomePage extends StatelessWidget {
             BigCard(pair: pair),
             ElevatedButton(
               onPressed: () {
-                print('button pressed!');
+                appState.getNext();
               },
               child: Text('Next'),
             ),
