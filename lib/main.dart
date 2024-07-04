493749ca5b74f1e6ffdf7f9465249bfdc7d7bd2d
Danh <handoiditu1508@gmail.com>
2024-07-04 09:59:32 +0700

    add floating appbar

--- lib/main.dart
+++ lib/main.dart
@@ -23,11 +23,25 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatelessWidget {
   const MyHomePage({super.key});
 
+  static const String title = 'Flutter Demo Home Page';
+
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
       body: CustomScrollView(
-        slivers: [],
+        slivers: [
+          SliverAppBar(
+            // Provide a standard title.
+            title: Text(title),
+            // Allows the user to reveal the app bar if they begin scrolling
+            // back up the list of items.
+            floating: true,
+            // Display a placeholder widget to visualize the shrinking size.
+            flexibleSpace: Placeholder(),
+            // Make the initial height of the SliverAppBar larger than normal.
+            expandedHeight: 200,
+          ),
+        ],
       ),
     );
   }
