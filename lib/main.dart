fb16841f0df44c903395d304289a7af878c9660f
Danh <handoiditu1508@gmail.com>
2024-07-08 10:45:11 +0700

    Create content for each tab

--- lib/main.dart
+++ lib/main.dart
@@ -39,6 +39,14 @@ class MyHomePage extends StatelessWidget {
               Tab(icon: Icon(Icons.directions_bike)),
             ],
           ),
+          title: const Text('App Bar'),
+        ),
+        body: const TabBarView(
+          children: [
+            Icon(Icons.directions_car),
+            Icon(Icons.directions_transit),
+            Icon(Icons.directions_bike),
+          ],
         ),
       ),
     );
