071142ad84ee0a1a91f148e5b245b54728d67acd
Danh <handoiditu1508@gmail.com>
2024-07-08 13:56:51 +0700

    Add a button that launches the selection screen

--- lib/main.dart
+++ lib/main.dart
@@ -37,3 +37,32 @@ class HomeScreen extends StatelessWidget {
     );
   }
 }
+
+class SelectionButton extends StatefulWidget {
+  const SelectionButton({super.key});
+
+  @override
+  State<SelectionButton> createState() => _SelectionButtonState();
+}
+
+class _SelectionButtonState extends State<SelectionButton> {
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      onPressed: () {
+        _navigateAndDisplaySelection(context);
+      },
+      child: const Text('Pick an option, any option!'),
+    );
+  }
+
+  Future<void> _navigateAndDisplaySelection(BuildContext context) async {
+    // Navigator.push returns a Future that completes after calling
+    // Navigator.pop on the Selection Screen.
+    final result = await Navigator.push(
+      context,
+      // Create the SelectionScreen in the next step.
+      MaterialPageRoute(builder: (context) => const SelectionScreen()),
+    );
+  }
+}
