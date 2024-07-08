5444ff6055bdf31810e0ee85c27a4122b5355af0
Danh <handoiditu1508@gmail.com>
2024-07-08 10:31:32 +0700

    Create a TabController

--- lib/main.dart
+++ lib/main.dart
@@ -28,25 +28,9 @@ class MyHomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '0',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
+    return const DefaultTabController(
+      length: 3,
+      child: Scaffold(),
     );
   }
 }
