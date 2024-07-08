a58b224dc332e16b336cde14bc67f73a2f857978
Danh <handoiditu1508@gmail.com>
2024-07-08 11:53:32 +0700

    Navigate and pass data to the detail screen

--- lib/main.dart
+++ lib/main.dart
@@ -54,6 +54,14 @@ class TodosScreen extends StatelessWidget {
         itemBuilder: (context, index) {
           return ListTile(
             title: Text(todos[index].title),
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => DetailScreen(todo: todos[index]),
+                ),
+              );
+            },
           );
         },
       ),
