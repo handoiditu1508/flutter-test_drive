9777a6305c1eff13158e87bd21cbb47645da6561
Danh <handoiditu1508@gmail.com>
2024-07-08 14:01:34 +0700

    Show the selection screen with two buttons

--- lib/main.dart
+++ lib/main.dart
@@ -66,3 +66,41 @@ class _SelectionButtonState extends State<SelectionButton> {
     );
   }
 }
+
+class SelectionScreen extends StatelessWidget {
+  const SelectionScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Pick an option'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(8),
+              child: ElevatedButton(
+                onPressed: () {
+                  // Pop here with "Yep"...
+                },
+                child: const Text('Yep!'),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8),
+              child: ElevatedButton(
+                onPressed: () {
+                  // Pop here with "Nope"...
+                },
+                child: const Text('Nope.'),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
