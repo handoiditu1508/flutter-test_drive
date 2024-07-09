a0068e3d2be3cfc3fcacd8c5ee47063947f5a529
Danh <handoiditu1508@gmail.com>
2024-07-09 14:38:03 +0700

    Create Block class to store data

--- lib/data.dart
+++ lib/data.dart
@@ -21,6 +21,19 @@ class Document {
   }
 }
 
+class Block {
+  final String type;
+  final String text;
+  Block(this.type, this.text);
+
+  factory Block.fromJson(Map<String, dynamic> json) {
+    if (json case {'type': final type, 'text': final text}) {
+      return Block(type, text);
+    }
+    throw const FormatException('Unexpected JSON format');
+  }
+}
+
 const documentJson = '''
 {
   "metadata": {
