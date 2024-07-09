c93a3e0ca6a6d673569a052112681c21eb8c9f29
Danh <handoiditu1508@gmail.com>
2024-07-09 15:26:46 +0700

    Add formatting logic for weeks

--- lib/main.dart
+++ lib/main.dart
@@ -34,6 +34,10 @@ class DocumentScreen extends StatelessWidget {
       Duration(inDays: 0) => 'today',
       Duration(inDays: 1) => 'tomorrow',
       Duration(inDays: -1) => 'yesterday',
+      Duration(inDays: final days) when days > 7 =>
+        '${days ~/ 7} weeks from now',
+      Duration(inDays: final days) when days < -7 =>
+        '${days.abs() ~/ 7} weeks ago',
       Duration(inDays: final days, isNegative: true) =>
         '${days.abs()} days ago',
       Duration(inDays: final days) => '$days days from now',
