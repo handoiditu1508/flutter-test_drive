4638ff3ad0c5a171b5a0996599a808596ede0668
Danh <handoiditu1508@gmail.com>
2024-07-10 10:50:59 +0700

    starter code

--- lib/main.dart
+++ lib/main.dart
@@ -1,41 +1,55 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MyApp());
+const owlUrl =
+    'https://raw.githubusercontent.com/flutter/website/main/src/content/assets/images/docs/owl.jpg';
+
+class FadeInDemo extends StatefulWidget {
+  const FadeInDemo({super.key});
+
+  @override
+  State<FadeInDemo> createState() => _FadeInDemoState();
+}
+
+class _FadeInDemoState extends State<FadeInDemo> {
+  @override
+  Widget build(BuildContext context) {
+    return ListView(children: <Widget>[
+      const Image(image: AssetImage("assets/owl.jpg")),
+      TextButton(
+        child: const Text(
+          'Show Details',
+          style: TextStyle(color: Colors.blueAccent),
+        ),
+        onPressed: () => {},
+      ),
+      const Column(
+        children: [
+          Text('Type: Owl'),
+          Text('Age: 39'),
+          Text('Employment: None'),
+        ],
+      ),
+    ]);
+  }
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+    return const MaterialApp(
+      home: Scaffold(
+        body: Center(
+          child: FadeInDemo(),
+        ),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
 
-class MyHomePage extends StatelessWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(title),
-      ),
-      body: const Center(
-        child: Text('You have pushed the button this many times:'),
-      ),
-    );
-  }
+void main() {
+  runApp(
+    const MyApp(),
+  );
 }
