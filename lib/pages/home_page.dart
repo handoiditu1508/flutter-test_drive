f2a0c3d40ad263dd5dd5a1bfe8c305f64fdcc4a2
Danh <handoiditu1508@gmail.com>
2024-07-18 14:48:12 +0700

    simplify AppBar

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -5,53 +5,31 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return DefaultTabController(
-      length: 3,
-      child: Scaffold(
-        appBar: AppBar(
-          leading: IconButton(
+    return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(
+          onPressed: () {},
+          icon: const Icon(Icons.menu),
+        ),
+        title: const Flexible(
+          child: Text(
+            "Ecommerce",
+          ),
+        ),
+        actions: [
+          IconButton(
             onPressed: () {},
-            icon: const Icon(Icons.menu),
+            icon: const Icon(Icons.notifications),
           ),
-          title: const Flexible(
-            fit: FlexFit.tight,
-            child: Text(
-              "Ecommerce",
-            ),
-          ),
-          actions: [
-            IconButton(
-              onPressed: () {},
-              icon: const Icon(Icons.search),
-            ),
-            IconButton(
-              onPressed: () {},
-              icon: const Icon(Icons.notifications),
-            ),
-          ],
-          bottom: const TabBar(tabs: [
-            Tab(
-              icon: Icon(Icons.cloud_outlined),
-              text: "Cloud",
-            ),
-            Tab(
-              icon: Icon(Icons.beach_access_sharp),
-              text: "Beach",
-            ),
-            Tab(
-              icon: Icon(Icons.brightness_5_sharp),
-              text: "Sunny",
-            ),
-          ]),
-        ),
-        body: ListView(
-          children: [
-            for (var i = 0; i < 10; i++)
-              const Placeholder(
-                fallbackHeight: 200,
-              )
-          ],
-        ),
+        ],
+      ),
+      body: ListView(
+        children: [
+          for (var i = 0; i < 10; i++)
+            const Placeholder(
+              fallbackHeight: 200,
+            )
+        ],
       ),
     );
   }
